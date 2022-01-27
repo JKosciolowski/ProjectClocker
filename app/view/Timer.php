ffea60a7dd2e326baa579a4f11746fa9a7ab3b26
bv48655 <bv48655@zut.edu.pl>
2022-01-27 13:57:43 +0100

    edited

--- app/view/Timer.php
+++ app/view/Timer.php
@@ -3,14 +3,14 @@
     <h1 class="timer-title">Timer</h1>
     <div class="timer">
         <h3 class="timer-text"></h3>
-        <input type="hidden" id="current_task" value="<?php echo isset($data['task_in_progress']['start_date']) ? strtotime($data['task_in_progress']['start_date']) * 1000 : 0; ?>" />
-        <input type="hidden" id="current_task_id" value="<?php echo isset($data['task_in_progress']['taskId']) ? $data['task_in_progress']['start_date'] : 0; ?>" />
-        <input type="hidden" id="task_name" value="<?php echo isset($data['task_in_progress']['task_name']) ? $data['task_in_progress']['task_name'] : 0; ?>" />
+        <input type="hidden" id="current_project" value="<?php echo isset($data['project_in_progress']['project_date']) ? strtotime($data['project_in_progress']['start_date']) * 1000 : 0; ?>" />
+        <input type="hidden" id="current_project_id" value="<?php echo isset($data['project_in_progress']['projectId']) ? $data['project_in_progress']['start_date'] : 0; ?>" />
+        <input type="hidden" id="project_name" value="<?php echo isset($data['project_in_progress']['project_name']) ?? $data['project_in_progress']['project_name'] : 0; ?>" />
     </div>
-    <input name="task_name" id="task_name_input" type="text" placeholder="Wprowadź nazwę projektu"/>
+    <input name="task_name" id="project_name_input" type="text" placeholder="Wprowadź nazwę projektu"/>
     <button counting="0" class="timer-button">Start</button>
     <div class="timer_history">
-        <?php foreach ($data['archived_tasks'] as $archived_project) {
+        <?php foreach ($data['archived_projects'] as $archived_project) {
             $interval = date_diff(date_create($archived_project['start_date']), date_create($archived_project['end_date']));
             $date = new DateTime($archived_project['start_date']);
         ?>
